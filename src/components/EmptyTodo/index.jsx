a48f53d32ecd63d8c9a4e6375baab7deb0f83d72
Luis Agreda <10234971+LuisAgreda@users.noreply.github.com>
2024-08-22 15:08:29 -0500

    feat: added component to show an empty status

---
+++ src/components/EmptyTodo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function EmptyTodo() {
+  return (
+    <p className="empty-todo">
+      Crea tu primer TODO
+    </p>
+  )
+}
+
+export { EmptyTodo }
